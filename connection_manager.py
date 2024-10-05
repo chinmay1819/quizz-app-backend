ab04f506fac688dd17b80b4576f3a9b223ca9fe9
Chinmay Deshpande <chinmay.deshpande@iauro.com>
2024-10-06 00:05:49 +0530

    feat: added functions for different event operatins

--- connection_manager.py
+++ connection_manager.py
@@ -35,12 +35,42 @@ class ConnectionManager:
             response_json = {
                 "answer":message
             }
+            logging.info(response_json)
             await connection.send_bytes(json.dumps(response_json))
 
+    async def broadcast_new_problem(self, message: bytes):
+        for connection in self.active_connections:
+            logging.info(f" Message sent to sender : {connection}")
+            response_json = {
+                "problem":message
+            }
+            logging.info(response_json)
+            await connection.send_bytes(json.dumps(response_json))
+
+    async def broadcast_winner(self,message:bytes):
+        for connection in self.active_connections:
+            logging.info(f"Declaring winner")
+            response_json = {
+                "winner":message
+            }
+            logging.info(response_json)
+            await connection.send_bytes(json.dumps(response_json))
+    
     async def send_current_problem(self, websocket: WebSocket, redis):
         """Send the current math problem to the connected WebSocket."""
         current_problem = await redis.get("current_problem")
         if current_problem:
-            await websocket.send_text(f"Current Problem: {current_problem}")
+            # await websocket.send_text(f"Current Problem: {current_problem}")
+            response_json = {
+                "problem":current_problem
+            }
+            logging.info(response_json)
+            await websocket.send_bytes(json.dumps(response_json))
+
         else:
-            await websocket.send_text("No problem available currently.")
+            # await websocket.send_text("No problem available currently.")
+            response_json = {
+                "problem_not_available":"No problem available currently"
+            }
+            logging.info(response_json)
+            await websocket.send_bytes(json.dumps(response_json))
--- services/quiz_service.py
+++ services/quiz_service.py
@@ -1,7 +1,9 @@
+import json
 import random
 import logging
 import asyncio
 from fastapi import WebSocket
+from connection_manager import ConnectionManager
 from utils.constants import Constants
 
 class QuizService:
@@ -16,25 +18,37 @@ class QuizService:
         await redis.set("current_problem", problem)
         await redis.set("current_solution", solution)
         await redis.delete("current_winner")  # Reset the winner key
+        logging.info("New problem created")
         return problem
 
 
     @staticmethod
-    async def process_answer(redis,websocket:WebSocket,manager,answer,username):
+    async def process_answer(redis,websocket:WebSocket,manager:ConnectionManager,answer,username):
         solution = await redis.get("current_solution")
         current_winner = await redis.get("current_winner")
         if current_winner:
-            await websocket.send_text(Constants.winner_already_declared)
+            # await websocket.send_text(Constants.winner_already_declared)
+            response_json = {
+                "issue":Constants.winner_already_declared
+            }
+            await websocket.send_bytes(json.dumps(response_json))
             return
         if answer and int(answer) == int(solution):
             is_first = await redis.setnx("current_winner",username)
             if is_first:
-                await manager.broadcast(f" Winner:{username}")
-                logging.info(f"Winner is {username}")
+                await manager.broadcast_winner(username)
+                logging.info(f"Winner is {username}") 
                 await asyncio.sleep(3)
                 new_problem = await QuizService.generate_new_problem(redis=redis)
-                await manager.broadcast(f"New problem {new_problem}")
+                await manager.broadcast_new_problem(new_problem)
             else:
-                await websocket.send_text(Constants.winner_already_declared)
+                response_json = {
+                "issue":Constants.winner_already_declared
+                }
+                await websocket.send_bytes(json.dumps(response_json))
         else:
-            await websocket.send_text(Constants.incorrect_answer)
+            response_json = {
+                "issue":Constants.incorrect_answer
+            }
+            await websocket.send_bytes(json.dumps(response_json))
+            # await websocket.send_text(Constants.incorrect_answer)
